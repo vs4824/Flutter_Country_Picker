ac15dd932c00eca7564dad2db9ea0a7396395d13
Vaibhav Sharma <vaibhav.2022@tecorb.co>
2023-05-17 16:42:16 +0530

    Flutter Country Picker

---
+++ android/app/src/main/kotlin/com/example/flutter_country_picker/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.flutter_country_picker
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,108 @@
+import 'package:country_picker/country_picker.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Demo for country picker package',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      supportedLocales: const [
+        Locale('en'),
+        Locale('ar'),
+        Locale('es'),
+        Locale('de'),
+        Locale('fr'),
+        Locale('el'),
+        Locale('et'),
+        Locale('nb'),
+        Locale('nn'),
+        Locale('pl'),
+        Locale('pt'),
+        Locale('ru'),
+        Locale('hi'),
+        Locale('ne'),
+        Locale('uk'),
+        Locale('hr'),
+        Locale('tr'),
+        Locale('lv'),
+        Locale('lt'),
+        Locale('ku'),
+        Locale('nl'),
+        Locale('it'),
+        Locale.fromSubtags(
+            languageCode: 'zh',
+            scriptCode: 'Hans'), // Generic Simplified Chinese 'zh_Hans'
+        Locale.fromSubtags(
+            languageCode: 'zh',
+            scriptCode: 'Hant'), // Generic traditional Chinese 'zh_Hant'
+      ],
+      localizationsDelegates: [
+        CountryLocalizations.delegate,
+        GlobalMaterialLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+      ],
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Demo for country picker')),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            showCountryPicker(
+              context: context,
+              //Optional.  Can be used to exclude(remove) one ore more country from the countries list (optional).
+              exclude: <String>['KN', 'MF'],
+              favorite: <String>['SE'],
+              //Optional. Shows phone code before the country name.
+              showPhoneCode: true,
+              onSelect: (Country country) {
+                print('Select country: ${country.displayName}');
+              },
+              // Optional. Sets the theme for the country list picker.
+              countryListTheme: CountryListThemeData(
+                // Optional. Sets the border radius for the bottomsheet.
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(40.0),
+                  topRight: Radius.circular(40.0),
+                ),
+                // Optional. Styles the search field.
+                inputDecoration: InputDecoration(
+                  labelText: 'Search',
+                  hintText: 'Start typing to search',
+                  prefixIcon: const Icon(Icons.search),
+                  border: OutlineInputBorder(
+                    borderSide: BorderSide(
+                      color: const Color(0xFF8C98A8).withOpacity(0.2),
+                    ),
+                  ),
+                ),
+                // Optional. Styles the text in the search field
+                searchTextStyle: TextStyle(
+                  color: Colors.blue,
+                  fontSize: 18,
+                ),
+              ),
+            );
+          },
+          child: const Text('Show country picker'),
+        ),
+      ),
+    );
+  }
+}
